1c0edaa6e8b32266882ef52bee8d50f19e83f857
Susanne Danninger <susanne.danninger@gmx.at>
2026-01-11 22:13:43 +0100

    add ansible for index.html creation

---
+++ on-prem/web/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+cat > /usr/share/nginx/html/index.html <<'EOF'
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Traefik LB Test</title>
+  </head>
+  <body>
+    <h1>Served by: __HOSTNAME__</h1>
+    <p>Refresh the page to see load balancing across containers.</p>
+  </body>
+</html>
+EOF
+
+sed -i "s/__HOSTNAME__/$(hostname)/g" /usr/share/nginx/html/index.html
+exec nginx -g 'daemon off;'
